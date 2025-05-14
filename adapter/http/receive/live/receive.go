e3d55433370b3a438283a710315442494288ff8d
FeiUmbrella <2199492464@qq.com>
2025-05-14 08:35:14 +0800

    chore(live):定义用于接收推拉流json信息的结构体

--- adapter/http/receive/live/receive.go
+++ adapter/http/receive/live/receive.go
@@ -10,17 +10,28 @@ type GetLiveRoomInfoReceiveStruct struct {
 }
 
 type LivestatRes struct {
-	Status int `json:"status"`
-	Data   struct {
-		Publishers []struct {
-			Key             string `json:"key"`
-			Url             string `json:"url"`
-			StreamId        int    `json:"stream_id"`
-			VideoTotalBytes int64  `json:"video_total_bytes"`
-			VideoSpeed      int    `json:"video_speed"`
-			AudioTotalBytes int    `json:"audio_total_bytes"`
-			AudioSpeed      int    `json:"audio_speed"`
-		} `json:"publishers"`
-		Players interface{} `json:"players"`
-	} `json:"data"`
+	//Status int `json:"status"`
+	//Data   struct {
+	//	Publishers []struct {
+	//		Key             string `json:"key"`
+	//		Url             string `json:"url"`
+	//		StreamId        int    `json:"stream_id"`
+	//		VideoTotalBytes int64  `json:"video_total_bytes"`
+	//		VideoSpeed      int    `json:"video_speed"`
+	//		AudioTotalBytes int    `json:"audio_total_bytes"`
+	//		AudioSpeed      int    `json:"audio_speed"`
+	//	} `json:"publishers"`
+	//	Players interface{} `json:"players"`
+	//} `json:"data"`
+	HTTPFLV struct {
+		Servers []struct {
+			Applications []struct {
+				Live struct {
+					Streams []struct {
+						Name string `json:"name"` // 这个就是推流码 room-${uid}
+					} `json:"streams"`
+				} `json:"live"`
+			} `json:"applications"`
+		} `json:"servers"`
+	} `json:"http-flv"`
 }
